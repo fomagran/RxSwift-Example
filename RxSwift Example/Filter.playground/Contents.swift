3c9453f64b82384055474f5068d3da7329541d82
Fomagran <fomagran6@naver.com>
2021-01-26 12:16:52 +0900

    skip,skipWhile,skipUntil

--- RxSwift Example/Filter.playground/Contents.swift
+++ RxSwift Example/Filter.playground/Contents.swift
@@ -5,14 +5,39 @@ let strikes = PublishSubject<String>()
 
 let disposeBag = DisposeBag()
 
-//element index를 정해주면
-strikes.elementAt(2)
-    .subscribe(onNext:{_ in print("You are out!")})
+//skip(숫자) 숫자만큼 스킵하고 방출한다.
+Observable.of(1,2,3,4,5,6)
+    .skip(3)
+    .subscribe(onNext:{
+        print($0)
+    }).disposed(by: disposeBag)
+
+////skipWhile 정해진 필터를 스킵하고 그 다음부터 모든 것을 방출. ex) 짝수가 오면 스킵해
+Observable.of(2,2,2,4,5,6)
+    .skipWhile { $0%2 == 0}
+    .subscribe(onNext:{
+        print($0)
+    })
     .disposed(by: disposeBag)
+//5
+//6
 
-strikes.onNext("X")
-strikes.onNext("X")
-//정해준 index의 element가 방출된다.
-strikes.onNext("X")
 
+//skipUntil은 정해진 옵저버블이 방출될때까지 스킵한다.
+let subject = PublishSubject<Int>()
+let trigger = PublishSubject<Int>()
+
+subject
+    .skipUntil(trigger)
+    .subscribe(onNext:{print($0)}
+    ).disposed(by: disposeBag)
+
+
+
+subject.onNext(1)
+subject.onNext(2)
+//아무것도 출력안됨.
+trigger.onNext(2)
+subject.onNext(3)
+//3
 
