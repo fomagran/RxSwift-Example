d2b4ff6927c558af850d6b3fdd2bd8dc15a73bd1
Fomagran <fomagran6@naver.com>
2021-01-26 12:23:09 +0900

    take,takeWhile,takeUntil skip과 반대로 작용함

--- RxSwift Example/Filter.playground/Contents.swift
+++ RxSwift Example/Filter.playground/Contents.swift
@@ -1,43 +1,36 @@
 import UIKit
 import RxSwift
 
-let strikes = PublishSubject<String>()
 
 let disposeBag = DisposeBag()
 
-//skip(숫자) 숫자만큼 스킵하고 방출한다.
-Observable.of(1,2,3,4,5,6)
-    .skip(3)
-    .subscribe(onNext:{
-        print($0)
-    }).disposed(by: disposeBag)
-
-////skipWhile 정해진 필터를 스킵하고 그 다음부터 모든 것을 방출. ex) 짝수가 오면 스킵해
-Observable.of(2,2,2,4,5,6)
-    .skipWhile { $0%2 == 0}
-    .subscribe(onNext:{
-        print($0)
-    })
+//take 숫자만큼만 방출한다.
+Observable.of(1,1,1,4,5,6)
+    .take(3)
+    .subscribe(onNext:{print($0)})
     .disposed(by: disposeBag)
-//5
-//6
 
+//takeWhile은 정해진 조건이 되기 전까지만 방출함.
+Observable.of(1,1,1,4,5,6)
+    .takeWhile({$0%2 == 0})
+    .subscribe(onNext:{print($0)})
+    .disposed(by: disposeBag)
 
-//skipUntil은 정해진 옵저버블이 방출될때까지 스킵한다.
 let subject = PublishSubject<Int>()
 let trigger = PublishSubject<Int>()
 
-subject
-    .skipUntil(trigger)
-    .subscribe(onNext:{print($0)}
-    ).disposed(by: disposeBag)
-
-
+subject.takeUntil(trigger)
+    .subscribe(onNext:{print($0)})
+    .disposed(by: disposeBag)
 
 subject.onNext(1)
 subject.onNext(2)
-//아무것도 출력안됨.
-trigger.onNext(2)
+//1
+//2
+trigger.onNext(1)
 subject.onNext(3)
-//3
+//방출되지 않음 skip과 반대로 작용
+
+
+
 
