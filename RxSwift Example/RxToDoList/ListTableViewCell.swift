0be58892d0ac1348bff4ad21470d56ce00f4566b
Fomagran <fomagran6@naver.com>
2021-01-27 10:40:54 +0900

    ToDoList 기본 UI세팅

---
+++ RxSwift Example/RxToDoList/ListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ListTableViewCell.swift
+//  RxSwift Example
+//
+//  Created by Fomagran on 2021/01/27.
+//
+
+import UIKit
+
+class ListTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var label:UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+     
+    }
+
+}
---
+++ RxSwift Example/RxToDoList/ListViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ListViewController.swift
+//  RxSwift Example
+//
+//  Created by Fomagran on 2021/01/27.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+    
+    @IBOutlet weak var segment:UISegmentedControl!
+    @IBOutlet weak var table:UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
+
+
+extension ListViewController:UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = table.dequeueReusableCell(withIdentifier: "ListTableViewCell") as! ListTableViewCell
+        return cell
+    }
+    
+    
+}
