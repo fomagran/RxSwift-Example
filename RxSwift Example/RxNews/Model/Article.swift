c39b948098e17afbe65167fafcfb5b24428902e6
Fomagran <fomagran6@naver.com>
2021-01-28 12:29:17 +0900

    urlRequest extension 만들기 및 articleList 모델에서 extension 만들기

--- RxSwift Example/RxNews/Model/Article.swift
+++ RxSwift Example/RxNews/Model/Article.swift
@@ -11,8 +11,15 @@ struct ArticlesList:Decodable {
     let articles:[Article]
 }
 
+extension ArticlesList {
+    static var all:Resource<ArticlesList> = {
+       let url = URL(string: TOP_HEADLINE_URL)!
+        return Resource(url: url)
+    }()
+}
+
 //모델을 만들때 타입 뒤에 ?붙여줘야 데이터가 표시된다.
 struct Article:Decodable{
-    let title:String?
+    let title:String
     let description:String?
 }
--- RxSwift Example/RxNews/NewsTableViewController.swift
+++ RxSwift Example/RxNews/NewsTableViewController.swift
@@ -21,23 +21,16 @@ class NewsTableViewController: UITableViewController {
     }
     
     private func populateNews() {
-        let url = URL(string: TOP_HEADLINE_URL)!
         
-        Observable.just(url)
-            .flatMap { url -> Observable<Data> in
-                let request = URLRequest(url: url)
-                return URLSession.shared.rx.data(request: request)
-            }.map{ data -> [Article]? in
-                return try? JSONDecoder().decode(ArticlesList.self, from: data).articles
-            }.subscribe(onNext:{ [weak self] articles in
-                if let articles = articles {
-                    self?.articles = articles
+        URLRequest.load(resource: ArticlesList.all)
+            .subscribe(onNext:{ [weak self]result in
+                if let result = result {
+                    self?.articles = result.articles
                     DispatchQueue.main.async {
                         self?.tableView.reloadData()
                     }
                 }
-            })
-            .disposed(by: disposeBag)
+            }).disposed(by: disposeBag)
       
     }
     
