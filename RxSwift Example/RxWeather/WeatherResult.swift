b293789a8a4685f5f72197b94df3f021038f66e3
Fomagran <fomagran6@naver.com>
2021-02-05 10:54:49 +0900

    혼자서 URLRequest 에러핸들링

--- RxSwift Example/RxWeather/WeatherResult.swift
+++ RxSwift Example/RxWeather/WeatherResult.swift
@@ -6,19 +6,3 @@
 //
 
 import Foundation
-
-struct WeatherResult:Decodable {
-    let main:Weather
-}
-
-//옵셔널일 경우 새로운 웨더를 만들어서 넣어버린다.
-extension WeatherResult {
-    static  var empty:WeatherResult {
-        return WeatherResult(main: Weather(temp: 0.0, humidity: 0.0))
-    }
-}
-
-struct Weather:Decodable {
-    let temp:Double
-    let humidity:Double
-}
--- RxSwift Example/URLRequest+Extensions.swift
+++ RxSwift Example/URLRequest+Extensions.swift
@@ -16,18 +16,19 @@ struct Resource<T:Decodable> {
 //RxNews를 만들땐 Observable<T?>로 만들어서 옵셔널로 줄것
 extension URLRequest {
     
-    static func load<T>(resource:Resource<T>) -> Observable<T?> {
+    static func load<T>(resource:Resource<T>) -> Observable<T>  {
         return Observable.just(resource.url)
-            .flatMap{
-                url -> Observable<Data> in
+            .flatMap{ url -> Observable<(response:HTTPURLResponse,data:Data)> in
                 let request = URLRequest(url: url)
-                return URLSession.shared.rx.data(request: request)
+                return URLSession.shared.rx.response(request: request)
+            }.map{
+                response,data -> T in
+                if 200..<300 ~= response.statusCode {
+                    return try! JSONDecoder().decode(T.self, from: data)
+                }else{
+                    throw RxCocoaURLError.httpRequestFailed(response: response, data: data)
+                }
             }
-            .map{
-                data -> T? in
-                return try? JSONDecoder().decode(T.self, from: data)
-            }
-            .asObservable()
     }
 }
 
